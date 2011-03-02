e5a1ea7a3b308dabff1792968019910bc120b64e
sander1 <sanderspies+plexapp@gmail.com>
2011-03-02 03:30:33 +0100

    Login fix

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -6,9 +6,12 @@
 #  Copyright (c) 2009 Plex. All rights reserved.
 #
 
+import time
+from base64 import b64encode
+
 SITEMAP_URL   = "http://www.screencastsonline.com/sco_info/info/sitemapcategory.html"
 RSS_URL_FREE  = "http://www.screencastsonline.com/feeds/scofree.xml"
-RSS_URL_2011  = "http://www.screencastsonline.com/Extra_Premium/feeds/scoextra_prem_2011_HD.xml"
+RSS_URL_2011  = "http://www.screencastsonline.com/Extra_Premium/feeds/scoextra_prem_2011_%s.xml"
 RSS_URL_2010  = "http://www.screencastsonline.com/Extra_Premium/feeds/scoextra_prem_2010_%s.xml"
 RSS_URL_2009  = "http://www.screencastsonline.com/Extra_Premium/feeds/scoextra_prem_2009_%s.xml"
 RSS_URL_2008  = "http://www.screencastsonline.com/Extra_Premium/feeds/scoextra_prem_2008_%s.xml"
@@ -17,52 +20,46 @@ RSS_URL_2006  = "http://www.screencastsonline.com/Extra_Premium/feeds/scoextra_p
 RSS_URL_2005  = "http://www.screencastsonline.com/Extra_Premium/feeds/scoextra_prem_2005_mixed.xml"
 BLACKLIST_URL = "http://www.screencastsonline.com/Extra_Premium/feeds/scoblacklist"
 
-LOGGED_IN = False
-
+####################################################################################################
 def Start():
   Plugin.AddPrefixHandler("/video/sco", MainMenu, "ScreenCastsOnline", "icon-default.png", "art-default.jpg")
   Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
+
   MediaContainer.viewGroup = "List"
-  MediaContainer.noCache = True
   MediaContainer.art = R("art-default.jpg")
+
   DirectoryItem.thumb = R("icon-sco.png")
   VideoItem.thumb = R("icon-sco.png")
   PrefsItem.thumb = R("icon-sco.png")
-  HTTP.CacheTime = 7200
-  #Prefs.SetTitle1Title("ScreenCastsOnline Preferences")
-  if Dict["blacklist"] is None: Dict.Reset()
-  LogIn()
-  SetTitle1()
 
+  HTTP.CacheTime = 7200
+  if Dict["blacklist"] is None: Dict.Reset()
+
+####################################################################################################
+def AuthHeader():
+  header = {}
+
+  if Prefs["username"] and Prefs["password"]:
+    header = {"Authorization": "Basic " + b64encode(Prefs["username"] + ":" + Prefs["password"])}
+    Log(header)
+
+  return header
+
+####################################################################################################
 def SetTitle1():
-  if LOGGED_IN:
+  if LoggedIn():
     MediaContainer.title1 = "ScreenCastsOnline Extra!"
   else:
     MediaContainer.title1 = "ScreenCastsOnline"
 
-def CreateDict():
-  Dict["blacklist"] = []
-
+####################################################################################################
 def GetVideoDef():
   if Prefs["hd"]: return "HD"
-  else: return "ED"
+  else: return "ipod" # Used to be "ED"
 
-def UpdateCache():
-  if LOGGED_IN:
-    videoDef = GetVideoDef()
-    HTTP.Request(RSS_URL_2005)
-    HTTP.Request(RSS_URL_2006 % videoDef)
-    HTTP.Request(RSS_URL_2007 % videoDef)
-    HTTP.Request(RSS_URL_2008 % videoDef)
-    HTTP.Request(RSS_URL_2009 % videoDef)
-    HTTP.Request(RSS_URL_2010 % videoDef)
-    HTTP.Request(RSS_URL_2011)
-    UpdateBlacklist()
-  else:
-    HTTP.Request(RSS_URL_FREE)
-
+####################################################################################################
 def UpdateBlacklist():
-  blacklist_json = HTTP.Request(BLACKLIST_URL).content
+  blacklist_json = HTTP.Request(BLACKLIST_URL, headers=AuthHeader()).content
   if blacklist_json is not None:
     try:
       blacklist_json = blacklist_json.replace(',\n]', '\n]')
@@ -72,45 +69,34 @@ def UpdateBlacklist():
   else:
     Dict["blacklist"] = []
 
-def LogIn():
-  global LOGGED_IN
-  username = Prefs["username"]
-  password = Prefs["password"]
-  if (username != "" and password != ""):
-    try:
-      # Set the password & request a members-only page
-      HTTP.SetPassword("www.screencastsonline.com", Prefs["username"], Prefs["password"])
-      result = HTTP.Request("http://www.screencastsonline.com/Extra_Premium/info/overview.html", cacheTime=0).content
-      if result is not None:
-        LOGGED_IN = True
-        UpdateBlacklist()
-        return True
-      else:
-        LOGGED_IN = False
-        return False
-    except:
-      LOGGED_IN = False
-      return False
-  LOGGED_IN = False
-  return False
+####################################################################################################
+def LoggedIn():
+  try:
+    result = HTTP.Request("http://www.screencastsonline.com/Extra_Premium/info/overview.html#" + str(time.time()), cacheTime=0, headers=AuthHeader()).content
+    UpdateBlacklist()
+    Log(" --> Login successful")
+    return True
+  except:
+    Log(" --> Login failed")
+    return False
 
+####################################################################################################
 def ValidatePrefs():
-  LogIn()
-  Thread.Create(UpdateCache)
-  SetTitle1()
-  if not LOGGED_IN:
-    return MessageContainer("Unable to log in to ScreenCastsOnline", "The username or password you provided was incorrect.")
+  if LoggedIn() == False:
+    return MessageContainer("Unable to log in to ScreenCastsOnline", "The username or password you provided is incorrect.")
 
+####################################################################################################
 def MainMenu():
-  d = MediaContainer()
+  SetTitle1()
+  d = MediaContainer(noCache=True)
 
   # Main menu code for Extra! members
-  if LOGGED_IN:
+  if LoggedIn():
     # Get the video definition
     videoDef = GetVideoDef()
 
     # Add all the items, using the appropriate feeds
-    d.Append(Function(DirectoryItem(RSSDirectory, title="ScreenCasts from 2011: SCO0283 and above"), url=RSS_URL_2011, label="2011"))
+    d.Append(Function(DirectoryItem(RSSDirectory, title="ScreenCasts from 2011: SCO0283 and above"), url=RSS_URL_2011 % videoDef, label="2011"))
     d.Append(Function(DirectoryItem(RSSDirectory, title="ScreenCasts from 2010: SCO0232 - SCO0282"), url=RSS_URL_2010 % videoDef, label="2010"))
     d.Append(Function(DirectoryItem(RSSDirectory, title="ScreenCasts from 2009: SCO0181 - SCO0231"), url=RSS_URL_2009 % videoDef, label="2009"))
     d.Append(Function(DirectoryItem(RSSDirectory, title="ScreenCasts from 2008: SCO0131 - SCO0180"), url=RSS_URL_2008 % videoDef, label="2008"))
@@ -125,25 +111,27 @@ def MainMenu():
     d.Append(PrefsItem(title="Log in to access ScreenCasts for Extra! members only"))
   return d
 
+####################################################################################################
 def RSSDirectory(sender, url, label=None, mixed=False):
   # Check whether we should be using HD versions & create the added_titles array for mixed mode
   HD = Prefs["hd"]
+  logged_in = LoggedIn()
   added_titles = []
 
   # Set the container title
   dir = MediaContainer(title2=label)
 
   # Fetch the feed
-  feed = XML.ElementFromURL(url)
+  feed = XML.ElementFromURL(url, headers=AuthHeader())
 
   # Get the blacklist (only used when logged in)
-  if LOGGED_IN:
+  if LoggedIn():
     blacklist = Dict["blacklist"]
   else:
     blacklist = []
 
   # Iterate through the feed items
-  for item in feed.xpath("//rss/channel/item"):
+  for item in feed.xpath("/rss/channel/item/enclosure[contains(@type,'video')]/parent::item"): # Ignore audio, pdf, etc.
     # Track whether the item should be added
     should_add = False
 
@@ -162,7 +150,7 @@ def RSSDirectory(sender, url, label=None, mixed=False):
     if title.find("[iPod]") == -1 and (HD or not HD and title.find("[HD]") == -1):
 
       # If logged in, we need to do additional processing
-      if LOGGED_IN:
+      if logged_in:
 
         # If the title is blacklisted, stop
         if title not in blacklist:
@@ -187,11 +175,17 @@ def RSSDirectory(sender, url, label=None, mixed=False):
     # If the item should be added, add it
     if should_add:
       # Clean up the title first
-      title = title.replace("[HD]","").replace("[DT]","").replace("[ED]", "")
-      dir.Append(VideoItem(url, title=title, subtitle=subtitle, thumb=Function(GetThumb, url=thumb)))
+      title = title.replace("[HD]","").replace("[DT]","").replace("[ED]","").replace("[iPod]","")
+      dir.Append(Function(VideoItem(PlayVideo, title=title, subtitle=subtitle, thumb=Function(GetThumb, url=thumb)), url=url))
 
   return dir
 
+####################################################################################################
+def PlayVideo(sender, url):
+  HTTP.SetPassword("www.screencastsonline.com", Prefs["username"], Prefs["password"])
+  return Redirect(url)
+
+####################################################################################################
 def GetThumb(url):
   if url is not None:
     try:
